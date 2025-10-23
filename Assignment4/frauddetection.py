0721a289c69574cd5f6ced043e3ff1ad1343bba6
Dasara Rexha <119667338+dasaraaa@users.noreply.github.com>
2025-10-23 19:43:07 +0200

    updated fraud detection

--- Assignment4/frauddetection.py
+++ Assignment4/frauddetection.py
@@ -17,3 +17,17 @@ def normalize_address(address):
     for k, v in replacements.items():
         address = address.replace(k, v)
     return address
+
+
+def detect_fraud(orders):
+    address_map = {}
+    frauds = []
+
+    for order in orders:
+        addr_norm = normalize_address(order["address"])
+        t = datetime.strptime(order["timestamp"], "%Y-%m-%d %H:%M")
+        email = order["email"]
+
+        if addr_norm in address_map:
+            for prev in address_map[addr_norm]:
+                prev_id, prev_email, prev_time = prev
